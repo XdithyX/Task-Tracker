d1a626ca87428fec505e3f9492f9eac1e9c9e0f2
Adithya Ajith <4adhi007@gmail.com>
2022-05-22 17:11:43 +0530

    task tracker header and btn added

---
+++ src/components/Button.js
@@ -0,0 +1,9 @@
+
+
+function Button(props) {
+  return (
+    <div><button style={{backgroundColor :props.color}} className='btn'>ADD</button></div>
+  )
+}
+
+export default Button
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import Button from "./Button"
+
+const Header = () => {
+  return (
+    <Header className='header'>
+    <h1>task tracker
+    </h1>
+    <Button color= 'yellow'/>
+    
+    </Header>
+  )
+}
+
+export default Header
